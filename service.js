4751d327d7165c1202be7b9a24b630e8a0c5773d
TomatoBigBig <a17504190204@163.com>
2021-12-05 02:24:55 +0800

    init

---
+++ service.js
@@ -0,0 +1,22 @@
+let http = require('http')
+let users = [
+    {
+        id:111,
+        name:'aiwocao'
+    },
+    {
+        id:222,
+        name:'wonima'
+    }
+]
+let server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    if(req.url === '/api/users'){
+        res.end(JSON.stringify(users))
+    }else{
+        res.end('啥也不是')
+    }
+});
+server.listen(3001,() => {
+    console.log('start service')
+})
