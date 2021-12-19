db74fb0d5e1d1a65cc6e5470cfcaa5ff7edab28f
TomatoBigBig <a17504190204@163.com>
2021-12-19 23:10:35 +0800

    gx

--- service.js
+++ service.js
@@ -14,7 +14,7 @@ let server = http.createServer((req, res) => {
     if(req.url === '/api/users'){
         res.end(JSON.stringify(users))
     }else{
-        res.end('啥也不是hahafdzxfhahahiuwdfsdfyiuyiihaha')
+        res.end('aiwoqu 啥也不是')
     }
 });
 server.listen(3001,() => {
