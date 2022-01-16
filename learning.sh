b0744c53ca51e619176c2a76820d1d2d60e178ae
TomatoBigBig <a17504190204@163.com>
2022-01-16 20:54:50 +0800

    gx

--- learning.sh
+++ learning.sh
@@ -7,9 +7,9 @@ git clean -f
 echo "拉取新代码"
 git pull
 echo "开始执行构建"
-docker build -t learning .
+docker build -t learning:1.0 .
 echo "停止就容器并删除"
 docker stop learning-container
 docker rm learning-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name learning-container -d learning
+docker container run -p 3000:3000 --name learning-container -d learning:1.0
