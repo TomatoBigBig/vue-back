9a4d0b61cbe13bf41973b05254fa34c8d77c55d0
TomatoBigBig <a17504190204@163.com>
2022-01-16 22:15:24 +0800

    gx

--- learning.sh
+++ learning.sh
@@ -6,10 +6,12 @@ git reset --hard origin/main
 git clean -f
 echo "拉取新代码"
 git pull origin main
+echo "打包"
+npm run build
 echo "开始执行构建"
 docker build -t learning:1.0 .
 echo "停止就容器并删除"
 docker stop learning-container
 docker rm learning-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name learning-container -d learning:1.0
+docker container run -p 80:80 --name learning-container -d learning:1.0
